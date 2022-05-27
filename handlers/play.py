bc2bb35371287fea314c639c3b6bb21f82d7216f
Anes010 <98156562+Anes010@users.noreply.github.com>
2022-05-27 10:28:22 +0300

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -88,7 +88,7 @@ async def generate_cover(requested_by, title, views, duration, thumbnail):
 
 
 @Client.on_message(
-    command(["شغل"])
+    command(["شغل", "تشغيل" ])
     & filters.group
     & ~filters.edited
     & ~filters.forwarded
